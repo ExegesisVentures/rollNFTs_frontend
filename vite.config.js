d1e5ffcae13e5f4c484641795e823c20cf9f6e4c
ExegesisVentures <exegesisventures@protonmail.com>
2025-10-29 10:06:27 -0700

    fix: disable vite code splitting for blockchain libs
    Fixes Object.defineProperty runtime error by preventing automatic
    code splitting which breaks circular dependencies in blockchain libs

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,25 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    jsx: 'automatic'
+  },
+  define: {
+    'global': 'globalThis',
+    'process.env': {}
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis'
+      }
+    }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined, // Don't split - fixes blockchain lib circular deps
+      },
+    },
+  },
 })
